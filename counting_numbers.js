50624ee4b7431a440e62139c3e80f98bc6c72657
amaankhanakz <sameeramaan2014@gmail.com>
2023-08-04 12:29:34 +0530

    counting_unique_numbers

---
+++ counting_numbers.js
@@ -0,0 +1,20 @@
+// 1,1,2,2,3,3,4,4,5,6,7,8,8
+
+function countUnique(array) {
+  if (array.length > 0) {
+    let i = 0;
+
+    for (let j = 1; j < array.length; j++) {
+      if (array[i] != array[j]) {
+        i++;
+        array[i] = array[j];
+      }
+    }
+    return i + 1;
+  } else {
+    console.log("Error");
+  }
+}
+
+const ans = countUnique([1, 1, 2, 2, 3, 3, 4, 4, 5, 6, 7, 8, 8]);
+console.log(ans);
